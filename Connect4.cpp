34e1d2b9dd9d03421db60b68438e80a76a994db7
Noah Glassford <noahglassford@gmail.com>
2019-10-27 18:21:38 -0400

    Working build without Column max and no AI
    This build more or less works now,  just gonna add column capping and AI later

--- Connect4.cpp
+++ Connect4.cpp
@@ -1,11 +1,19 @@
-
-//#include "pch.h"
+/*
+For GDW Challenge 2, first semester, first year
+Basic Connect 4 game
+*/
+#include<conio.h> 
+#include <windows.h> 
 #include <iostream>
 #include <string>
+#include <limits> //For input verification
 #include <cstdlib> //For Random numbers in AI
+#undef max //Undefines max to use cin.ignore functions
 using namespace std;
 
-const string Title[6] = { " _____________________________________________________ ",
+HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE); // For use of SetConsoleTextAttribute() 
+
+const string Title[6] = { " _____________________________________________________ ", //Title Card
 						 "|   __    _                 ___    _____              |",
 						 "|  |  |  |_|___ ___     ___|  _|  |   __|___ _ _ ___  |",
 						 "|  |  |__| |   | -_|   | . |  _|  |   __| . | | |  _| |",
@@ -14,23 +22,36 @@ const string Title[6] = { " ____________________________________________________
 
 int startscreeninput = 0; //the input from the user on the start screen
 int levelAI = 0; //the input for the difficuty
-bool game = true;
-int player;
-int drop;
-const int maxColumn = 7;
-const int maxRow = 6;
-int boardNum[maxColumn][maxRow];
-string board[maxColumn][maxRow];
-string columns[maxRow] = { "  1  ","  2  ","  3  ","  4  ","  5  ","  6  " };
-const string boardSpace = "[( )]";
+bool game = true; //Used to run the game's loop
+int player; //Decides which player goes
+int drop; //Used to place pieces
+int turn = 0;//sets turn value to zero (for colour in draw board) 
+bool FullRow; //Used to get a new input if the row is full
+const int maxColumn = 6; //Defines board columns
+const int maxRow = 7; //defines board rows
+int boardNum[6][7];//Board Data
+string board[maxColumn][maxRow]; //Visuals
+string columns[7] = { "  1  ","  2  ","  3  ","  4  ","  5  ","  6  ", "  7  " }; //Identifies rows to the players
+const string boardSpace = "[( )]"; //Each grid piece to be printed
 
-int AiEasy();
-void getLevelAI();
-void twoPlayer();
-void OnePlayerEasy();
-void basicGame(int, int);
-void updateBoard();
-void drawBoard();
+
+int AiEasy(); //Returns random int for drop
+void WinCheck(); //For wincheck
+//int AiProcessing(); //Rturns the int that the AI will drop
+void startScreen(); //Runs start stuff
+void getLevelAI(); //Tells the game which ai to run
+void twoPlayer(); //2 player game loop
+void OnePlayerEasy(); //Easy AI
+void basicGame(int, int); //Runs the game
+void updateBoard(); //Updates board
+void drawBoard(); //Draws the board
+void VerifyInput(); //Checks if the user input is valid
+
+
+int main() { 
+	startScreen();
+	cout << "Thanks for Playing\n";
+}
 
 void startScreenOutput() {
 
@@ -41,25 +62,66 @@ void startScreenOutput() {
 
 	cout << "	      1 = Two Player\n";
 	cout << "	      2 = Single Player vs AI\n";
-	cout << "	      3 = EXIT\n";
+	cout << "              3 = Rules\n";
+	cout << "	      4 = EXIT\n";
 	cout << endl;
 }
 
-void startScreen() {
+void startScreen()
+{
 	startScreenOutput();
-
+	bool InputLoop{ true };
 	int gamemode;
-	cout << "Please type in either 1, 2 or 3 from the options above to coninue: ";
-	cin >> gamemode;
+	cout << "Please type in either 1, 2, 3 or 4 from the options above to coninue: ";
 
+	while (1)
+	{
+		cin >> gamemode;
+		if (cin.fail())
+		{
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			std::cout << "Invalid input, try again" << std::endl;
+		}
+		else
+		{
+			break;
+		}
+	}
 	if (gamemode == 1) {
 		twoPlayer();
+		InputLoop = false;
 	}
 	else if (gamemode == 2) {
 		getLevelAI();
+		InputLoop = false;
 	}
-	else if (gamemode != 1 && gamemode != 2 && gamemode != 3) {
-		cout << "INVALID ENTRY";
+	else if (gamemode == 4)
+	{
+		system("CLS");
+		std::cout << "Thanks for playing!";
+		exit(1);
+	}
+	else if (gamemode == 3)
+	{
+		string rulemove; //Just used to cin, doesn't do anything
+		system("CLS");
+		std::cout << "Here are the Rules:\n";
+		std::cout << endl;
+		std::cout << "Four in a row AKA Connect 4 is a game where your goal is connect 4 of your pieces \ntogether in a row.";
+		std::cout << " This goal can be done by having 4 consecutive pieces in a row\nhorizontally, vertically, or diagonally.";
+		std::cout << " The game will take turns allowing each\nplayer to place one piece in a turn.Your goal is to block your ";
+		std::cout << "opponents 4 in a\nrow while trying to get 4 of your piece in a row first.When it is your turn, type\nin an ";
+		std::cout << "integer from 1-7 which will represent which row to drop your piece in. If\nthe game is 2 player, then let ";
+		std::cout << "your opponent take his turn. If you are playing\nagainst AI, the AI will automatically place their piece";
+		std::cout << "after you make your move.\nThat is pretty much it.Thanks for reading and hope you enjoy the game :) ";
+		std::cout << "\nPress any key to go back & enter to go back to menu \n";
+		std::cin >> rulemove;
+		system("CLS");
+		startScreen();
+	}
+	else if (gamemode != 1 && gamemode != 2 && gamemode != 3 && gamemode != 4) {
+		cout << "Invalid input, try again \n";
 	}
 }
 
@@ -86,8 +148,8 @@ void getLevelAI() {
 }
 
 void setupBoard() {
-	for (int i = 0; i < maxColumn; i++) {
-		for (int j = 0; j < maxRow; j++) {
+	for (int i = 0; i <= 5; i++) {
+		for (int j = 0; j <= 6; j++) {
 			boardNum[i][j] = 0;
 			board[i][j] = boardSpace;
 		}
@@ -96,26 +158,25 @@ void setupBoard() {
 
 void twoPlayer() {
 	player = (rand() % 2) + 1;
+
 	cout << endl << "Player 1 will be using O pieces, Player 2 will be using 0 pieces." << '\n' << "Going first will be Player " << player << endl << endl;
 	setupBoard();
 	drawBoard();
-
-	while (game) {
-		cout << endl << "Player " << player << ", pick your row: ";
-		
-		cin >> drop;
-		cout << endl;
-		basicGame(drop, player);
-		updateBoard();
-		drawBoard();
-		//check win condition
-		switch (player) { //update current player
+	while (game) 
+	{
+			cout << endl << "Player " << player << ", pick your row: ";
+			VerifyInput();
+			cout << endl;
+			basicGame(drop, player);
+			updateBoard();
+			drawBoard();
+			WinCheck();
+			switch (player) { //update current player
 		case 1: player = 2; break;
 		case 2: player = 1; break;
 		}
 	}
 }
-
 void OnePlayerEasy() {
 	setupBoard();
 	player = 1; //Sets human player first
@@ -127,41 +188,39 @@ void OnePlayerEasy() {
 		if (player == 1)
 		{
 			cout << "Pick your row" << endl;
-			cin >> drop;
+			VerifyInput();
 			cout << endl;
 			basicGame(drop, player);
 			updateBoard();
 			drawBoard();
 			player = 2;
-			//checkWin
+			WinCheck();
 		}
 		else if (player == 2)
 		{
-			basicGame(AiEasy(), player);
+			
 			updateBoard();
 			drawBoard(); //Draws board for the player after the ai goes
 			player = 1;
-			//checkWin
+			WinCheck();
 		}
 		
 	}
 	//AI Game, human is Player 1
 }
 
-void basicGame(int columnSelected, int playerTurn) {
-	bool found = false;
-	int column = columnSelected - 1;
-	for (int i = maxColumn; i >= 0 && !found; i--) {
-
-		if (boardNum[i][column] == 0) {
-			boardNum[i][column] = player;
-			found = true;
+void basicGame(int columnSelected, int playerTurn)
+{	
+	
+	for (int i = 5; i >= -2; i--) 
+		{  
+			if (boardNum[i][drop - 1] == 0) {
+				boardNum[i][drop - 1] = player;
+				break;
+			}
 		}
-	}
-	//start at bottom of column, if its already occupied, go up to the next one
-
+	
 }
-
 void updateBoard() {
 	for (int i = 0; i < maxColumn; i++) {
 		for (int j = 0; j < maxRow; j++) {
@@ -169,7 +228,7 @@ void updateBoard() {
 				board[i][j] = "[(O)]";
 			}
 			else if (boardNum[i][j] == 2) {
-				board[i][j] = "[(0)]";
+				board[i][j] = "[(X)]";
 			}
 		}
 	}
@@ -177,7 +236,16 @@ void updateBoard() {
 
 void drawBoard() {
 	system("CLS"); //Clears Board to reduce Clutter
-	for (int i = 0; i < maxColumn; i++) {
+	if (turn % 2 == 0) {
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 12);// changes colour to Red 
+		turn++;
+	}
+	else
+	{
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);// changes colour to Yellow 
+		turn++;
+	}
+	for (int i = 0; i < 7; i++) {
 		cout << columns[i];
 	}
 	cout << "\n";
@@ -192,14 +260,110 @@ void drawBoard() {
 /*
 This functtion will be used for the easy ai, which will just place a random piece on its turn.
 */
-
 int AiEasy()
 {
 	drop = rand() % 7 + 1;
 	return drop;
 }
 
-int main() {
-	startScreen();
-	cout << "Thanks for Playing\n";
+
+void VerifyInput()
+{
+	while (1)
+	{
+		cin >> drop;
+		if (cin.fail())
+		{
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			std::cout << "Invalid input, try again" << std::endl;
+		}
+		if (((drop <= 7)) && drop > 0 )
+		{
+			break;
+			}	
+			else
+			{
+			std::cout << "Invalid input, try again \n";
+			}
+		}
+	}
+
+
+void WinCheck()
+{
+	//Vertical
+	for (int y = 0; y < maxColumn; y++)
+	{
+		for (int x = 0; x < maxRow; x++)
+		{
+			if (boardNum[y][x] == 1 && boardNum[y + 1][x] == 1 && boardNum[y + 2][x] == 1 && boardNum[y + 3][x] == 1)
+			{
+				system("CLS");
+				cout << "Player 1 wins";
+				game = false;
+			}
+			else if (boardNum[y][x] == 2 && boardNum[y + 1][x] == 2 && boardNum[y + 2][x] == 2 && boardNum[y + 3][x] == 2)
+			{
+				system("CLS");
+				cout << "Player 2 wins";
+				game = false;
+				break;
+			}
+		}
+	}
+		
+	
+
+	//  vertical plane
+	for (int x = 0; x < maxRow; x++)
+	{
+		for (int y = 0; y < maxColumn; y++)
+		{
+			if (boardNum[y][x] == 1 && boardNum[y][x + 1] == 1 && boardNum[y][x + 2] == 1 && boardNum[y][x + 3] == 1)
+			{
+				system("CLS");
+				cout << "Player 1 wins \n";
+				game = false;
+				break;
+			}
+			else if (boardNum[y][x] == 2 && boardNum[y][x + 1] == 2 && boardNum[y][x + 2] == 2 && boardNum[y][x + 3] == 2)
+			{
+				system("CLS");
+				cout << "\nPlayer 2 wins \n";
+				game = false;
+				break;
+			}
+		}
+	//Both Diagonals
+		for (int x = 0; x < (maxRow); x++)
+		{
+			for (int y = 0; y < (maxColumn); y++)
+			{
+				if ((boardNum[y][x] == 1 && boardNum[y + 1][x + 1] == 1 && boardNum[y + 2][x + 2] == 1 && boardNum[y + 3][x + 3] == 1)
+					|| (boardNum[y + 3][x] == 1 && boardNum[y + 2][x + 1] == 1 && boardNum[y + 1][x + 2] == 1 && boardNum[y][x + 3] == 1))
+				{
+					system("CLS");
+					cout << "\nPlayer 1 wins \n";
+					game = false;
+					break;
+				}
+			}
+			for (int x = 0; x < (maxRow); x++)
+			{
+				for (int y = 0; y < (maxRow); y++)
+				{
+					if ((boardNum[y][x] == 2 && boardNum[y + 1][x + 1] == 2 && boardNum[y + 2][x + 2] == 2 && boardNum[y + 3][x + 3] == 2)
+						|| (boardNum[y + 3][x] == 2 && boardNum[y + 2][x + 1] == 2 && boardNum[y + 1][x + 2] == 2 && boardNum[y][x + 3] == 2))
+					{
+
+						system("CLS");
+						cout << "\nPlayer 2 wins \n";
+						game = false;
+						break;
+					}
+				}
+			}
+		}
+	}
 }
